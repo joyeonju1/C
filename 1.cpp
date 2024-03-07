95122798396b4ba0a348189c67b81819116460e5
joyeonju1 <142647234+joyeonju1@users.noreply.github.com>
2024-03-07 20:54:24 +0900

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+	//printf("hello world");
+	/*
+	printf("hello \nC3coding\n");
+	printf("hello \bC3coding\n"); 
+	printf("hello \rC3coding\n");
+	printf("hello \tC3coding\n");
+	*/
+	/*
+	printf("12345678\n");
+	printf("ABCDEFG\n");
+	printf("¥Î«—πŒ±π\n");
+	printf("abcdefg\n");
+	printf("°›°ﬁ¢¿¢æ\n");
+	printf("!@#$%^&*\n");
+	printf("12345678\n");
+	*/
+	printf("%%Solar Tracking System%%\n");
+	printf("\\Smart Devces\\\n");
+	printf("\"Matrix\"\n");
+	printf("\'Hello Robot'\n");
+	
+	
+	
+	
+	
+	return 0;
+}
