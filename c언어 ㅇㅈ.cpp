c5824af8876fba58d9105e813d9c09031672da7f
joyeonju1 <142647234+joyeonju1@users.noreply.github.com>
2024-03-14 20:47:30 +0900

    Add files via upload

---
+++ c언어 ㅇㅈ.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	printf("나의 이름은 %s 입니다.\n","조연주");
+	printf("나는 %d살입니다.\n", 15);
+	printf("나의 혈액형은 %s입니다.\n","AB");
+	printf("나의 시력은 %.1lf,%.1lf 입니다.\n",0.6,0.3);
+
+
+
+
+
+	return 0;
+}
+
+
+
