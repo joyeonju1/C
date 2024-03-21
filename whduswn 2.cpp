efdc01ff66758c238245184d7bb2dbcf5ba87d93
joyeonju1 <142647234+joyeonju1@users.noreply.github.com>
2024-03-21 20:58:59 +0900

    Add files via upload

---
+++ whduswn 2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+	/*
+	int num;
+	printf("숫자를 하나 입력하세요. :");
+	scanf("%d",&num);
+	printf("%d\n",num);
+	*/
+	/*
+	double eye_l,eye_r;
+	printf("시력을 입력하세요:");
+	scanf("%lf %lf", &eye_r,&eye_l);
+	printf("오른쪽 시력:%lf\n왼쪽 시력:%lf",eye_r,eye_l);
+	*/
+	int a,b,c;
+	printf("학년 반 번호입력하세요 :");
+	scanf("%d %d %d",&a,&b,&c);
+	printf("%d학년 %d반 %d번호 입니다.",a,b,c);
+	
+	
+	return 0;
+}
