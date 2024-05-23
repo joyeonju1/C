1ed736c6d6f686930427dc65280072fcf3c92afb
joyeonju1 <142647234+joyeonju1@users.noreply.github.com>
2024-05-23 20:55:06 +0900

    Add files via upload

---
+++ whduswn7.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main()
+{/*
+	for(int i=6; i<=1; i--)
+	{
+		printf("\n%dÇÐ³â",i);
+
+		for(int j=1; j<=7; j++)
+		
+		{
+			printf("\n%d¹Ý",j);
+		}
+		printf("\n") 
+	}
+	*/
+	/*
+	for (int i=1; i<=5; i++)
+	{
+		for(int j=1; j<=i; j++)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+	*/
+	/*
+	for (int i=5; i>=1; i--)
+	{
+		for(int j=1; j<=i; j++)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+	*/
+	for (int i=5; i>=1; --i)
+	{
+		for(int j=5; j>=1; j++)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+	return 0;
+	
+}
